16ccdcfe34d7f96e60e8e170f184c2441bcc1030
NerKhoza <khoza.ner.142@gmail.com>
2023-11-30 13:00:16 -0800

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,7 @@
 from calculator_1 import add, sub, mul, div
 
 a = 10
+
 b = 5
 
 result_add = add(a, b)
@@ -12,7 +13,7 @@ result_divide = div(a, b)
 
 if __name__ == "__main__":
 
-    print(f" {a} + {b} = {result_add}")
-    print(f" {a} - {b} = {result_subtract}")
-    print(f" {a} * {b} = {result_multiply}")
-    print(f" {a} / {b} = {result_divide}")
+    print(f"{a} + {b} = {result_add}")
+    print(f"{a} + {b} = {result_subtract}")
+    print(f"{a} + {b} = {result_multiply}")
+    print(f"{a} + {b} = {result_divide}")
