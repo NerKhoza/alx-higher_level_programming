e0df15c087cfb765e4c70e4835cc36d605aa4690
NerKhoza <khoza.ner.142@gmail.com>
2023-11-30 14:36:57 -0800

    command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     import sys
-
     lenarg = len(sys.argv)
-
     if lenarg == 1:
         print("{} arguments.".format(lenarg - 1))
     else:
         print("{} arguments:".format(lenarg - 1))
-
-        for i in range(1, lenarg):
+    for i in range(1, lenarg):
             print("{} : {}".format(i, sys.argv[i]))
