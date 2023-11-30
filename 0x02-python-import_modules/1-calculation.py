b01d4957de0d232765309ed5d91988867cd67f20
NerKhoza <khoza.ner.142@gmail.com>
2023-11-30 13:22:50 -0800

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,14 +6,9 @@ a = 10
 
 b = 5
 
-result_add = add(a, b)
-result_subtract = sub(a, b)
-result_multiply = mul(a, b)
-result_divide = div(a, b)
-
 if __name__ == "__main__":
 
-    print(f"{a} + {b} = {result_add}")
-    print(f"{a} + {b} = {result_subtract}")
-    print(f"{a} + {b} = {result_multiply}")
-    print(f"{a} + {b} = {result_divide}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
