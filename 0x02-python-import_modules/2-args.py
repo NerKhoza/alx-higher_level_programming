5bdbb18a49c8484c8bd3fb8997207086861864f6
NerKhoza <khoza.ner.142@gmail.com>
2023-11-30 14:52:23 -0800

    command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if lenArg == 1:
         print("{} arguments.".format(lenArg - 1))
     elif lenArg == 2:
-        print("{} argument.".format(lenArg - 1))
+        print("{} argument:".format(lenArg - 1))
     else:
         print("{} arguments:".format(lenArg - 1))
     for i in range(1, lenArg):
