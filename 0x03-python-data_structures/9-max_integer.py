27f97c6e7360b8d1aad2275b7d1034d64cbb01b9
NerKhoza <khoza.ner.142@gmail.com>
2023-12-04 16:17:10 -0800

    nction that finds the biggest integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return (None)
-
-    max_value = my_list[0]
-    for num in my_list[1:]:
-        if num > max_value:
-            max_value = num
-
-            return max_value
+    new_list = []
+    if my_list:
+        my_list.sort(reverse = True)
+        return (my_list[0])
+    return (None)
