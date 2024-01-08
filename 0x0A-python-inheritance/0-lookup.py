96bdae76bbe578ce1d6cee6c61c25418a265e90d
NerKhoza <khoza.ner.142@gmail.com>
2024-01-08 10:20:44 -0800

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+
+"""Defines an object attribute lookup function"""
+
 def lookup(obj):
-    return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or not attr.startswith("__")]
+    """Return a list of an object's available attributes"""
+    return (dir(obj))
