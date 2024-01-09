f46365c0ec7609b1d860ad040f5d21fd040c7522
NerKhoza <khoza.ner.142@gmail.com>
2024-01-09 08:18:37 -0800

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a function that writes a string"""
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+        num_characters_written = len(text)
+        return num_characters_written
