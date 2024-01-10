84f553fa45b36d345902267dddf44c9ac5610b81
NerKhoza <khoza.ner.142@gmail.com>
2024-01-10 03:05:39 -0800

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function that Write a class MyInt"""
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        # Invert the behavior of ==
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        # Invert the behavior of !=
+        return not super().__ne__(other)
