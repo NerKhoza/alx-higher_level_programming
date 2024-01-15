457eb368c455713ca72d74b5c388648bad5b6a9b
NerKhoza <khoza.ner.142@gmail.com>
2024-01-15 11:57:08 -0800

    unittest

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,7 +5,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+        super().__init__(size,size, x, y, id)
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
