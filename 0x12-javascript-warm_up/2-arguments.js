ccd64fce76df14ec1f0b7b6e5f97e6d3be387090
NerKhoza <khoza.ner.142@gmail.com>
2024-03-14 02:32:56 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const numArg = process.argv.length - 2;
-if (numArg == 0){
+if (numArg == 0) {
 	console.log('No argument');
 }
-else if (numArg == 1){
+else if (numArg == 1) {
 	console.log('Argument found');
 }
-else{
+else {
 	console.log('Arguments found');
 }
