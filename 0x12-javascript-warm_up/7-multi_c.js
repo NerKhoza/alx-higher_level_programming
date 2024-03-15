70b08f0d560f0d0255335901df5d6f639af8f551
NerKhoza <khoza.ner.142@gmail.com>
2024-03-14 22:14:17 -0700

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+if (process.argv.length < 3) {
+	console.log('Missing number of occurrences');
+{
+else {
+	const x = parseInt(process.argv[2]);
+	if (!isNaN(x)) {
+		for (let i = 0; i < x; i++) {
+			console.log('C is fun');
+		}
+	}
+}
+else {
+	console.log('Missing number of occurrences');
+}
