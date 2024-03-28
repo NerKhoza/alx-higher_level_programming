bd6224c6990422a20feff46f596a65266cb1590c
NerKhoza <khoza.ner.142@gmail.com>
2024-03-28 15:59:04 -0700

    POST an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#  Python script that takes in a URL and an email address
+
+
+import requests
+import sys
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: ./script.py <URL> <email>")
+        return
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    print(f"Your email is: {email}")
+
+    response = requests.post(url, data={'email': email})
+
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
