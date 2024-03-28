7c02f2b5e4a19f15c6f6badc51041c0d779cfcca
NerKhoza <khoza.ner.142@gmail.com>
2024-03-28 16:16:26 -0700

    Error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+import requests
+import sys
+
+
+def get_url_content(url):
+    try:
+        response = requests.get(url)
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+        else:
+            print(response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    get_url_content(url)
