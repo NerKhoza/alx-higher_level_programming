440d157d9fdd80596d6710d8c15b192dc7c13707
NerKhoza <khoza.ner.142@gmail.com>
2024-03-28 14:56:58 -0700

    update

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#  Python script that takes in a URL
+
+
+import urllib.request
+import urllib.error
+import sys
+
+
+def fetch_url(url):
+
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python 3-error_code.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    fetch_url(url)
