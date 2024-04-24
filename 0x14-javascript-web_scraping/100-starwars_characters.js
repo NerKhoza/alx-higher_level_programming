68994a4a7fc90ea519228928b94386d50f8bd23a
NerKhoza <khoza.ner.142@gmail.com>
2024-04-23 19:03:35 -0700

    update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,26 +3,43 @@
 const request = require('request');
 
 const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a movie ID as the first argument.');
+  process.exit(1);
+}
+
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const movieData = JSON.parse(body);
-
-    console.log(`Characters of "${movieData.title}":`);
-
-    movieData.characters.forEach((characterUrl) => {
-      request(characterUrl, function (charError, charResponse, charBody) {
-        if (!charError && charResponse.statusCode === 200) {
-          const characterData = JSON.parse(charBody);
-	console.log(characterData.name);
-        } else {
-          console.error('Error fetching character data:', charError);
-        }
-      });
-    });
-  } else {
-    console.error('Error fetching movie data:', error);
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  const movieData = JSON.parse(body);
+  const charactersUrls = movieData.characters;
+
+  charactersUrls.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        process.exit(1);
+      }
+
+      if (response.statusCode !== 200) {
+        console.error('Unexpected status code:', response.statusCode);
+        process.exit(1);
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
 });
 
